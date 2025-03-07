e469b453c1198d711e67a9e916cdc9696ec138ad
unknown <sababarnovi@gmail.com>
2025-03-08 02:38:34 +0400

    dagvianebuli 16

---
+++ 16/categories/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from config.model_utils.models import TimeStampedModel
+
+class Category(TimeStampedModel, models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    products = models.ManyToManyField('products.Product', related_name='categories')
+
+    def __str__(self):
+        return f"{self.name}"
+
+    
+
+class CategoryImage(TimeStampedModel, models.Model):
+    image = models.ImageField(upload_to='categories/')
+    product = models.ForeignKey('categories.Category', related_name='images', on_delete=models.CASCADE)
+    is_active = models.BooleanField(default= False)
+
